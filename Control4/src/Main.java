5e3c9c1c8e2913a5d8e7c173dfbb1543f88303cf
fcsfelipe@gmail.com <fcsfelipe@gmail.com>
2013-06-12 11:23:06 -0400

    asdf

--- Control4/src/Main.java
+++ Control4/src/Main.java
@@ -5,7 +5,23 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Animal p = Madre.crearPerro("Fido",true); //Abstract Factor
+		Animal g = Madre.crearGato("Felix","rojo");
+
+		p.setEstado("perfecto");
+		g.setEstado("enfermo");
+
+		System.out.println(p.hablar());
+		System.out.println(g.hablar());
+
+		Vaterinario v = new Veterinario("vet");
+		System.out.println(v.curar(g)); //Adapter
+
+		JsonMagic.push(p); //Memento
+		JsonMagic.push(g);
+
+
 
 	}
 
