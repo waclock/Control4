bc9156363a1fb478d00b175954dd597a250a2b46
John Peebles <jpeeblesg@gmail.com>
2013-06-12 10:55:41 -0400

    pusheando

--- Control4/src/Madre.java
+++ Control4/src/Madre.java
@@ -5,8 +5,8 @@ public  class Madre {
 	public static Gato crearGato(String nombre,String color){
 		return new Gato(nombre,color);
 	}
-	public static Perro crearPerro(String nombre){
-		return new Perro(nombre);
+	public static Perro crearPerro(String nombre,boolean cola){
+		return new Perro(nombre,cola);
 	}
 
 }
