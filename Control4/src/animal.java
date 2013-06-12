b37b6e71cb44d1e6ef641a39c63188a8e82a7c36
fcsfelipe@gmail.com <fcsfelipe@gmail.com>
2013-06-12 11:05:47 -0400

    adf

--- Control4/src/animal.java
+++ Control4/src/animal.java
@@ -1,7 +1,17 @@
 public class Animal{
 	public String nombre;
+	public String estado;
+
 	public String hablar(){
 		return "HOLA";
 	}	
+
+	public String getEstado() {
+		return this.estado;
+	}
+
+	public void setEstado(String e) {
+		this.estado = e.toUpperCase();
+	}
 
 }
